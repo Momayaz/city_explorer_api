33ed6e7fa47762477a8cf56efd0cc6f41df3976b
momayaz <momayaz.dr.1@gmail.com>
2020-10-31 16:01:26 +0200

    lab07_is_done

--- server.js
+++ server.js
@@ -1,112 +1,96 @@
 'use strict';
+// Constructor
+function Location(city, locationData) {
+    this.search_query = city;
+    this.formated_query = locationData[0].display_name;
+    this.latitude = locationData[0].lat;
+    this.longitude = locationData[0].lon;
+}
+function Weather(data) {
+    this.forecast = data.weather.description;
+    this.time = data.datetime;
+}
+function Trail(locationData){
+    this.name=locationData.name;
+    this.location=locationData.location;
+    this.length=locationData.length;
+    this.stars= locationData.stars;
+    this.star_votes=locationData.starVotes;
+    this.summary=locationData.summary;
+    this.trail_url=locationData.trail_url;
+    this.conditions=locationData.conditions;
+    this.condition_date=locationData.conditionDate;
+    this.condition_time=locationData.condition_time;
 
+}
+// Defining Application Dependencies
 const express = require('express');
+const superagent = require('superagent');
 const cors = require('cors');
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3030;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const TRAIL_API_KEY=process.env.TRAIL_API_KEY;
 const app = express();
 app.use(cors());
-require('dotenv').config();
-const PORT = process.env.PORT;
-
-
-app.get('/location', (req,res)=>{
-    let city = req.query.city;
-    let locationData = require('./data/location.json');
-    let information = locationData[0];
-    let locationObject = new LocationInfo(city, information.display_name, information.lat, information.lon);
-    res.status(200).json(locationObject);
-})
-
-function LocationInfo(search_query, formetted_query, letitude,longitude){
-    this.search_query = search_query;
-    this.formetted_query = formetted_query;
-    this.letitude = letitude; 
-    this.longitude = longitude;
-}
-
-function Weather(forcast,time){
-    this.forcast = forcast;
-    this.time = time;
-}
-app.get('/weather', (req,res)=>{
-   
-    const weatherData = require('./data/weather.json');
-    let weatherInfo = [];
-    weatherData.data.forEach(element => {
-        let info = element.weather.description;
-        let time = element.datetime;
-        weatherInfo.push(new Weather(info,time));
-    });
-    let weatherObject = new Weather(weatherInfo);
-    res.status(200).json(weatherObject);
-    handlerError(res);
-})
-
-app.use('*', (req,res)=>{
-    res.status(404).send('something worng, try again')
-})
-
-app.listen(PORT, ()=>{
-    console.log(`Momayaz, app is listening to ${PORT}`)
-})
-
-function handlerError(res){
-    if(res === 500){
-        res.status(500).send('Sorry, something went wrong')
+// Routes
+app.get('/', welcomePage);
+app.get('/location', locationData);
+app.get('/weather', weatherData);
+app.get('/trails',trailData);
+app.use('*', notFound);
+function handlErrors(response) {
+    if (response.status === 500) {
+        response.status(500).send('Sorry, something went wrong');
     }
 }
+// Helpers
+function welcomePage(reqeust, response) {
+    response.status(200).send('Home Page Welcome to express');
+}
+function locationData(request, response) {
+    const city = request.query.city;
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
+    superagent.get(url).then(locationData => {
+        let location = new Location(city, locationData.body);
+        response.json(location);
+        // handlErrors(response);
+    }).catch(console.error);
+}
+function weatherData(request, response) {
+    const city = request.query.search_query;
+    const longitude=request.query.longitude;
+    const latitude=request.query.latitude;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+    let weatherArr = [];
+    superagent.get(url).then(locationData => {
+        locationData.body.data.map(data=>{
+            weatherArr.push( new Weather(data));
+        });
+    
+        response.json(weatherArr);
+        // handlErrors(response);
+    });
+    // .catch(console.error);
+}
+function trailData(request, response) {
+    const longitude=request.query.longitude;
+    const latitude=request.query.latitude;
+    const url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${TRAIL_API_KEY}`;
+    let trailArr = [];
+    superagent.get(url).then(locationData => {
+        locationData.body.trails.map((data)=>{
+            trailArr.push( new Trail(data));
+        });
 
+   
+        response.json(trailArr);
+        // handlErrors(response);
+    }).catch(console.error);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const cors = require('cors');
-
-// require('dotenv').config();
-
-// const app = express();
-// app.use(cors());
-// const PORT= process.env.PORT;
-
-
-// app.get('/location', (req,res)=>{
-//     let city = req.query.city;
-//     let getJson = require('./data/location.json');
-//     let jsonObject = getJson[0];
-//     let locationObject = new LocationFunc(city, jsonObject.display_name, jsonObject.lat, jsonObject.lon);
-//     res.status(200).json(locationObject);
-
-// })
-
-// function LocationFunc(search_query, formetted_query, letitude, longitude){
-//     this.search_query = search_query;
-//     this.formetted_query = formetted_query;
-//     this.letitude = letitude; 
-//     this.longitude = longitude;
-// }
-
-// app.listen(PORT, ()=>{
-//     console.log(`app is lestening to ${PORT}`)
-// })
+function notFound(request, resp) {
+  resp.status(404).send('Not found');
+}
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
